6ba0a82f283b66a33d01c49d3fcba6d44b8cc306
mohammad mi <mohammadgit7299@gmail.com>
2021-11-14 16:43:34 +0330

    create user class with members

---
+++ include.h
@@ -0,0 +1,22 @@
+#include<iostream>
+
+#ifndef INCLUDE_G
+#define INCLUDE_G
+
+
+class user
+{
+    string name;
+    unsigned int card_number;
+    unsigned short int opening_account_year;
+    unsigned short int opening_account_month;
+    unsigned short int opening_account_day;
+    unsigned short int expiration_account_year;
+    unsigned long long int balance = 0;
+    string * record_ptr = nullptr;
+    string * ip_lists_ptr = nullptr;
+};
+
+
+
+#endif
