b6ee89088226441bae372b51f86ed72dcf0b1ace
mohammad mi <mohammadgit7299@gmail.com>
2021-11-16 19:04:31 +0330

    change function of re_enter

--- task.cpp
+++ task.cpp
@@ -171,43 +171,44 @@ void Re_Enter_String(string & input_string , user * customer_cls)
 {
     unsigned short int chance = 3;
 
-    cout << endl << "Enter Command Under This Line ...." << endl;
-    fflush(stdin);
-    getline(cin , input_string);
-
-    while (input_string.empty())
+    while (true)
     {
-        system("cls");
+        cout << endl << "Enter Command Under This Line ...." << endl;
+        fflush(stdin);
+        getline(cin , input_string);
 
-        if (chance == 0)
+        while (input_string.empty())
         {
             system("cls");
-            cerr << "+     -     -     -     -     -     -     -     -     -     -     -     -     +" << endl;
-            cerr << "|                                                                             |" << endl;
-            cerr << "|    You Have  #  3  #  Invalid Attempts And We Have To Closs The Program     |" << endl;
-            cerr << "|                                                                             |" << endl;
-            cerr << "+     -     -     -     -     -     -     -     -     -     -     -     -     +" << endl;
-            exit(EXIT_FAILURE);
+
+            if (chance == 0)
+            {
+                system("cls");
+                cerr << "+     -     -     -     -     -     -     -     -     -     -     -     -     +" << endl;
+                cerr << "|                                                                             |" << endl;
+                cerr << "|    You Have  #  3  #  Invalid Attempts And We Have To Closs The Program     |" << endl;
+                cerr << "|                                                                             |" << endl;
+                cerr << "+     -     -     -     -     -     -     -     -     -     -     -     -     +" << endl;
+                exit(EXIT_FAILURE);
+            }
+            
+            cout << "!!!   You Have  ~  " << chance << "  ~  Chance To Enter Valid Command   !!!" << endl;
+            cout << "Order Can not Be Empty . Please Enter Command Under This Line ..." << endl;
+            fflush(stdin);
+            getline(cin ,input_string);
+            chance--;
         }
         
-        cout << "!!!   You Have  ~  " << chance << "  ~  Chance To Enter Valid Command   !!!" << endl;
-        cout << "Order Can not Be Empty . Please Enter Command Under This Line ..." << endl;
-        fflush(stdin);
-        getline(cin ,input_string);
-        chance--;
-    }
-    
-    if (input_string == "Exit" || input_string == "exit" || input_string == "EXIT" || input_string == "0")
-    {
-        system("cls");
-        cout << "!!   You Entered Exit Command In The First Of Program   !!" << endl;
-        cout << "\t!!   End Of Program   !!" << endl;
-        exit(EXIT_SUCCESS);
-    }
+        if (input_string == "Exit" || input_string == "exit" || input_string == "EXIT" || input_string == "0")
+        {
+            system("cls");
+            cout << "!!   You Entered Exit Command In The First Of Program   !!" << endl;
+            cout << "\t!!   End Of Program   !!" << endl;
+            exit(EXIT_SUCCESS);
+        }
 
-        while (input_string != "exit" || input_string != "Exit" || input_string != "0" || input_string != "EXIT")
-    {
         Check_User_Name(input_string ,customer_cls);
+
     }
 }
 
