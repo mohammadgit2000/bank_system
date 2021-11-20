c522b921714f9c317c11aaaa8f12849bda31b6ca
mohammad mi <mohammadgit7299@gmail.com>
2021-11-20 18:09:24 +0330

    cretae some functions and added command

--- inc/include.h
+++ inc/include.h
@@ -1,4 +1,3 @@
-
 #ifndef INCLUDE_G
 #define INCLUDE_G
 
@@ -9,30 +8,36 @@ using namespace std;
 class user
 {
     public :
-    void set_user_name(string);
-    string get_user_name();
-    void set_ip(string);
-    string get_ip();
-    user();
+    user(); // consttructore just set date of account like opening and expiration .
+    void set_user_name(string); // set username of account .
+    string get_user_name(); // return username of account .
+    void set_ip(string); // set ip of account .
+    string get_ip(); // return ip of account .
+    unsigned short int get_opening_year(); // return opening year of account .
+    unsigned short int  get_opening_month(); // return opening month of account .
+    unsigned short int get_opening_day(); // return opening day of account .
+    unsigned short int get_expiration_year(); // return expiratoin of account .
 
     private :
-    string user_name = "empty" ;
-    unsigned int card_number;
-    unsigned short int opening_account_year;
-    unsigned short int opening_account_month;
-    unsigned short int opening_account_day;
-    unsigned short int expiration_account_year;
-    unsigned long long int balance = 0;
-    string * record_ptr = nullptr;
-    string * ip_lists_ptr = nullptr;
-    string ip_of_account = "no_ip";
+    string user_name = "empty" ; // The username is stored in this variable .
+    unsigned int card_number; // The card number is stored in this variable .
+    string ip_of_account = "no_ip"; // stored ip of account .
+    unsigned short int opening_account_year; // stored opening yeaar of account .
+    unsigned short int opening_account_month; // stored opening month of account .
+    unsigned short int opening_account_day; // stored opening day of account .
+    unsigned short int expiration_account_year; // stored opening expiration of account .
+    unsigned long long int balance = 0; // stored balance of account .
+    string * record_ptr = nullptr; // stored all of records .
+    string * ip_lists_ptr = nullptr; // stored extra ip`s .
 };
 
 
-void Check_User_Namr(string & ,user *&);
-void Re_Enter_String(string & ,user *&);
-user * Increase_Class(user *&);
-void Print_Eror(string ,string);
-bool Is_Command_Entred(string &  , unsigned short int & );
+void Check_User_Namr(string & ,user *&); // username and ip validation .
+void Re_Enter_String(string & ,user *&); // if entreance information was wrong we call this function again .
+user * Increase_Class(user *&); // increase size of class array and copy pre class`s information in new class array that increased .
+void Print_Eror(string ,string); // recive two string and print them in format of eror .
+bool Is_Command_Entred(string &  , unsigned short int & ); // if command exist in the entrance string return true else return false .
+bool Is_Char_Exist(char , string & ,unsigned short int , bool ); // if character of word exist in index of string we return true .
+bool Is_First_Bigger_Number(int ,int); // if first number bigger than second number we return true .
 
 #endif
